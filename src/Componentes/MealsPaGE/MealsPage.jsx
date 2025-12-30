7f7759fae87cb6914532f36b5207a913cd905491
hakimcolor <hakimcolor777@gmail.com>
2025-12-30 20:42:42 +0600

    i change my button style .. the see details button dising

--- src/Componentes/MealsPaGE/MealsPage.jsx
+++ src/Componentes/MealsPaGE/MealsPage.jsx
@@ -128,7 +128,7 @@ const MealsPage = () => {
                 </div>
                 <button
                   onClick={() => handleSeeDetails(meal._id)}
-                  className="mt-4 bg-red-600 text-white px-4 py-2 w-full rounded hover:bg-red-700 transition cursor-pointer"
+                  className="mt-4 bg-red-600 text-white px-4 py-2 w-full rounded hover:bg-red-700 transition cursor-pointer border rounded-2xl"
                 >
                   See Details
                 </button>
